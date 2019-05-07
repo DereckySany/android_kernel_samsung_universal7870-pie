d4ef953c6601bb1f17f9037bc7954d5d4ede5941
yEsPaP <h.sachmanovas@gmail.com>
2019-05-07 15:24:35 +0000

    add j7x firmware

---
+++ firmware/coreriver/.tc305k_j7top_kor.fw.gen.o.cmd
@@ -0,0 +1,9 @@
+cmd_firmware/coreriver/tc305k_j7top_kor.fw.gen.o := ../toolchain/bin/aarch64-linux-android-gcc -Wp,-MD,firmware/coreriver/.tc305k_j7top_kor.fw.gen.o.d  -nostdinc -isystem /home/yolo/toolchain/bin/../lib/gcc/aarch64-linux-android/4.9.x/include -I./arch/arm64/include -Iarch/arm64/include/generated  -Iinclude -I./arch/arm64/include/uapi -Iarch/arm64/include/generated/uapi -I./include/uapi -Iinclude/generated/uapi -include ./include/linux/kconfig.h -D__KERNEL__ -mlittle-endian  -D__ASSEMBLY__ -fno-PIE -Wa,-gdwarf-2         -c -o firmware/coreriver/tc305k_j7top_kor.fw.gen.o firmware/coreriver/tc305k_j7top_kor.fw.gen.S
+
+source_firmware/coreriver/tc305k_j7top_kor.fw.gen.o := firmware/coreriver/tc305k_j7top_kor.fw.gen.S
+
+deps_firmware/coreriver/tc305k_j7top_kor.fw.gen.o := \
+
+firmware/coreriver/tc305k_j7top_kor.fw.gen.o: $(deps_firmware/coreriver/tc305k_j7top_kor.fw.gen.o)
+
+$(deps_firmware/coreriver/tc305k_j7top_kor.fw.gen.o):
---
+++ firmware/tsp_melfas/.mms438_a3xpre_init.fw.gen.o.cmd
@@ -0,0 +1,9 @@
+cmd_firmware/tsp_melfas/mms438_a3xpre_init.fw.gen.o := ../toolchain/bin/aarch64-linux-android-gcc -Wp,-MD,firmware/tsp_melfas/.mms438_a3xpre_init.fw.gen.o.d  -nostdinc -isystem /home/yolo/toolchain/bin/../lib/gcc/aarch64-linux-android/4.9.x/include -I./arch/arm64/include -Iarch/arm64/include/generated  -Iinclude -I./arch/arm64/include/uapi -Iarch/arm64/include/generated/uapi -I./include/uapi -Iinclude/generated/uapi -include ./include/linux/kconfig.h -D__KERNEL__ -mlittle-endian  -D__ASSEMBLY__ -fno-PIE -Wa,-gdwarf-2         -c -o firmware/tsp_melfas/mms438_a3xpre_init.fw.gen.o firmware/tsp_melfas/mms438_a3xpre_init.fw.gen.S
+
+source_firmware/tsp_melfas/mms438_a3xpre_init.fw.gen.o := firmware/tsp_melfas/mms438_a3xpre_init.fw.gen.S
+
+deps_firmware/tsp_melfas/mms438_a3xpre_init.fw.gen.o := \
+
+firmware/tsp_melfas/mms438_a3xpre_init.fw.gen.o: $(deps_firmware/tsp_melfas/mms438_a3xpre_init.fw.gen.o)
+
+$(deps_firmware/tsp_melfas/mms438_a3xpre_init.fw.gen.o):
